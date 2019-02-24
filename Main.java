252788ac31daa8c45e94ff8035b6a513749f5ee7
Yash Singhal <yashgyy@gmail.com>
2019-02-24 18:30:13 +0530

    Final Pass

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 class BadPassword extends RuntimeException{
@@ -11,70 +13,79 @@ class BadPassword extends RuntimeException{
 
 public class Main{
 
+    //Individual Method for each of constraint, can modify if needs
+
     public static boolean minimumLength(String var){
+        
         return var.length()<6;
 
     }
 
-    public static boolen maximumLength(String var){
+    public static boolean maximumLength(String var){
         return var.length()>12;
     }
 
-    public static boolen lowerCase(String str){
-        return !str.matches("[a-z]");
+    public static boolean lowerCase(String str){
+        return !Pattern.compile("[a-z]").matcher(str).find();
 
     }
 
     public static boolean upperCase(String str){
-        return !str.matches("[A-Z]");
+        
+        return !Pattern.compile("[A-Z]").matcher(str).find();
     }
 
     public static boolean Digit(String str){
-        return !str.matches("[0-9]");
+        return !Pattern.compile("[0-9]").matcher(str).find();
     }
 
     public static boolean Misc1(String str){
-        return !str.matches("[*&_#=@]");
+        return !Pattern.compile("[*$_#=@]").matcher(str).find();
     }
 
     public static boolean Misc2(String str){
-        return str.matches("[%!()]");
+        return Pattern.compile("[%!()]").matcher(str).find();
     }
 
     public static void main(String[] args) {
         Scanner In=new Scanner(System.in);
-        System.out.print("Enter Passwords seperated by Comma\n");
+        System.out.println("Enter Passwords seperated by Comma");
         String name=In.nextLine();
         String[] passwords=name.split(",");
         System.out.println(Arrays.toString(passwords));
+        System.out.println("--------------------------------------------");
         
         for(int i=0;i<passwords.length;i++){
-          
-            String Checker="Failure\n";
-            String First="Failure\n";
+            System.out.print("Given String is "+passwords[i]+"\n");
+            String Checker=new String("Failure\n");
+            String First=new String("Failure\n");
 
             
             try {
                 String Check=passwords[i];
                  switch(1){
-                     case 1: if(minimumLength(Check)){First+="Password Must be 6 character Long\n";}
-                     case 2: if(maximumLength(Check)){First+="Password Must not be longer than 12 characters\n";}
-                     case 3: if(lowerCase(Check)){First+="Password must contain one charcter from a-z\n";}
-                     case 4: if(upperCase(Check)){First+="Password must Contain one charcater from A-Z\n";}
-                     case 5: if(Digit(Check)){First+="Password must contain one character from 0-9";}
-                     case 6: if(Misc1(Check)){First+="Password must contain one character from *$_#=@";}
-                     case 7: if(Misc2(str)){First+="Password Must not contain a character from %)(!";}
+                     //Case Statements Check each of the conditions Fall Through
+                     case 1: if(minimumLength(Check)){First+="** Password Must be 6 character Long\n";}
+                     case 2: if(maximumLength(Check)){First+="** Password Must not be longer than 12 characters\n";}
+                     case 3: if(lowerCase(Check)){First+="** Password must contain one charcter from a-z\n";}
+                     case 4: if(upperCase(Check)){First+="** Password must Contain one charcater from A-Z\n";}
+                     case 5: if(Digit(Check)){First+="** Password must contain one character from 0-9\n";}
+                     case 6: if(Misc1(Check)){First+="** Password must contain one character from *$_#=@\n";}
+                     case 7: if(Misc2(Check)){First+="** Password Must not contain a character from %)(!\n";}
                  }
 
                  if(First.equals(Checker)){
-                     throw new BadPassword(First);
-                } else{
                     System.out.println("Success");
+                    System.out.println("------");
+                } else{
+                    throw new BadPassword(First);  //An Exception is thrown if any condition fails
+                    
                 }
 
 
             } catch (BadPassword e) {
-                System.out.println(e.getMessage());
+                System.out.print(e.getMessage());
+                System.out.println("------");
             }
         }
 
